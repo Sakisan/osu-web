09ad99b3d3f452a8d8d75202c34ba78f3ddf5f81
Antoine <antoine.snyers@skynet.be>
2016-03-16 12:35:08 +0100

    Make readability improvement to UserSeeder.php

--- database/seeds/ModelSeeders/UserSeeder.php
+++ database/seeds/ModelSeeders/UserSeeder.php
@@ -1,10 +1,27 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
+    private $faker;
+
+    private $improvement_speeds;
+
+    private $common_countries;
+
+    public function __construct()
+    {
+        $this->faker = $this->faker = Faker::create();
+        $this->common_countries = $this->common_countries = ['US', 'JP', 'CN', 'DE', 'TW', 'RU', 'KR', 'PL', 'CA', 'FR', 'BR', 'GB', 'AU'];
+        $this->improvement_speeds = $this->improvement_speeds = [
+            rand(100, 102) / 100, // Slow Learner
+            rand(102, 110) / 100, // Fast Learner
+            rand(110, 115) / 100, // Genius / Multiaccounter
+        ];
+    }
+
     /**
      * Run the database seeds.
      *
@@ -19,83 +36,64 @@ class UserSeeder extends Seeder
         // DB::table('osu_user_stats_taiko')->delete();
         // DB::table('osu_user_performance_rank')->delete();
 
-        $this->faker = Faker::create();
-
-        // Store some constants
-        $this->improvement_speeds = [
-            (rand(100, 110) / 100), // Fast Learner
-            (rand(100, 102) / 100), // Slow Learner
-            (rand(100, 115) / 100), // Genius / Multiaccounter :P
-        ];
-        $this->common_countries = ['US', 'JP', 'CN', 'DE', 'TW', 'RU', 'KR', 'PL', 'CA', 'FR', 'BR', 'GB', 'AU'];
 
         // Create 10 users and their stats
-        factory(App\Models\User::class, 10)->create()->each(function ($u) {
-
-            // USER STATS
-            $country_code = array_rand_val($this->common_countries);
-
-            $rank0 = rand(1, 500000);
-            $rank1 = rand(1, 500000);
-            $rank2 = rand(1, 500000);
-            $rank3 = rand(1, 500000);
-            $st = $u->statisticsOsu()->save(factory(App\Models\UserStatistics\Osu::class)->create(['country_acronym' => $country_code, 'rank' => $rank0, 'rank_score_index' => $rank0]));
-            $st1 = $u->statisticsOsu()->save(factory(App\Models\UserStatistics\Taiko::class)->create(['country_acronym' => $country_code, 'rank' => $rank1, 'rank_score_index' => $rank1]));
-            $st2 = $u->statisticsOsu()->save(factory(App\Models\UserStatistics\Fruits::class)->create(['country_acronym' => $country_code, 'rank' => $rank2, 'rank_score_index' => $rank2]));
-            $st3 = $u->statisticsOsu()->save(factory(App\Models\UserStatistics\Mania::class)->create(['country_acronym' => $country_code, 'rank' => $rank3, 'rank_score_index' => $rank3]));
-            // END USER STATS
-
-            // RANK HISTORY
+        factory(App\Models\User::class, 10)->create()->each(function ($user) {
 
             // Create rank histories for all 4 modes
-            for ($c = 0; $c <= 3; $c++) {
-                switch ($c) {
-                    case 0: $rank = $st->rank; break;
-                    case 1: $rank = $st1->rank; break;
-                    case 2: $rank = $st2->rank; break;
-                    case 3: $rank = $st3->rank; break;
-                    default: $rank = $st->rank;
-                }
+            for ($mode = 0; $mode <= 3; $mode++) { // 0 = standard, 1 = taiko etc...
 
-                $hist = new App\Models\RankHistory;
+                $statistics = $this->createStatistics($user, $mode);
 
-                $hist->mode = $c; // 0 = standard, 1 = taiko etc...
+                $rank = $statistics->rank;
 
-                $play_freq = rand(10, 35); // How regulary the user plays (as a % chance per day)
+                $rankHistory = new App\Models\RankHistory;
+                $rankHistory->mode = $mode;
 
                 // Start with current rank, and move down (back in time) to r0
-                $hist->r89 = $rank;
+                $rankHistory->r89 = $rank;
 
-                for ($i = 88; $i >= 0; $i--) {
-                    $r = 'r'.$i;
-                    $prev_r = 'r'.($i + 1);
-                    $prev_rank = $hist->$prev_r;
+                for ($day = 88; $day >= 0; $day--) {
+                    $r = 'r' . $day;
+                    $prev_r = 'r' . ($day + 1);
                     // We wouldn't expect the user to improve every day
-                    $does_improve = $this->faker->boolean($play_freq);
+                    $does_improve = $this->faker->boolean(rand(10, 35));
                     if ($does_improve === true) {
-                        $extreme_improvement = $this->faker->boolean(2); // chance of extreme improvement today
-                        if ($extreme_improvement === true) {
-                            $improvement_modifier = 1.5;
-                        } else {
-                            $improvement_modifier = array_rand_val($this->improvement_speeds);
-                        }
-                        $new_rank = round($hist->$prev_r * $improvement_modifier);
-                        if ($new_rank < 1) {
-                            $new_rank = 1;
-                        }
-                        $hist->$r = $new_rank;
+                        $improvement_modifier = array_rand_val($this->improvement_speeds);
+                        $new_rank = round($rankHistory->$prev_r * $improvement_modifier);
                     } else {
-                        $new_rank = round($hist->$prev_r * (rand(998, 999) / 1000));
-                        if ($new_rank < 1) {
-                            $new_rank = 1;
-                        }
-                        $hist->$r = $new_rank; // Slight decay
+                        $slightDecay = rand(998, 999) / 1000;
+                        $new_rank = round($rankHistory->$prev_r * $slightDecay);
                     }
+                    $new_rank = max($new_rank, 1);
+                    $rankHistory->$r = $new_rank;
                 }
-                $u->rankHistories()->save($hist);
+                $user->rankHistories()->save($rankHistory);
             }
-            // END RANK HISTORY
 
-        }); // end each user
+        });
+    }
+
+    function createStatistics($user, $mode)
+    {
+        if ($mode === 0) {
+            $modeClass = App\Models\UserStatistics\Osu::class;
+        } else if ($mode === 1) {
+            $modeClass = App\Models\UserStatistics\Taiko::class;
+        } else if ($mode === 2) {
+            $modeClass = App\Models\UserStatistics\Fruits::class;
+        } else { // if ($mode === 3) {
+            $modeClass = App\Models\UserStatistics\Mania::class;
+        }
+
+        $rank = rand(1, 50000);
+        $country_code = array_rand_val($this->common_countries);
+        $attributes = [
+            'country_acronym' => $country_code,
+            'rank' => $rank,
+            'rank_score_index' => $rank
+        ];
+        $object = factory($modeClass)->create($attributes);
+        return $user->statisticsOsu()->save($object);
     }
 }
